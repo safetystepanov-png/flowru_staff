ec167d000f7a0f9f290e32c6fc2e8157419eb4ab
Aleksandr <safetystepanov@gmail.com>
2026-04-15 14:37:25 +0300

    Switch iOS startup from storyboard and scene to explicit Flutter engine

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,23 +3,11 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  lazy var flutterEngine = FlutterEngine(name: "flowru_engine")
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    flutterEngine.run()
-    GeneratedPluginRegistrant.register(with: flutterEngine)
-
-    self.window = UIWindow(frame: UIScreen.main.bounds)
-    self.window?.rootViewController = FlutterViewController(
-      engine: flutterEngine,
-      nibName: nil,
-      bundle: nil
-    )
-    self.window?.makeKeyAndVisible()
-
-    return true
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
